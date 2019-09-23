26e564ad0811c9fbe3a2dff3fc8a5fdc8203645d
Kevin Quick <kquick@galois.com>
2019-09-23 15:13:38 -0700

    Obtain requests and dependencies from thespian director environment instead of packaged deps.

--- hydra/sysconfig.nix
+++ hydra/sysconfig.nix
@@ -178,9 +178,9 @@ rec {
       serviceConfig = {
         Type = "forking";
         ExecStartPre="${pkgs.bash}/bin/bash ${briareus_thespian_director}/install_to ${thespian_director_dir}";
-        ExecStart="${pkgs.python37.withPackages (pp: [ pp.thespian pp.setproctitle ])}/bin/python -m thespian.director start";
+        ExecStart="${pkgs.python37.withPackages (pp: [ pp.thespian pp.setproctitle pp.requests ])}/bin/python -m thespian.director start";
         ExecStartPost="${pkgs.coreutils}/bin/sleep 10";
-        ExecStop="${pkgs.python37.withPackages (pp: [ pp.thespian pp.setproctitle ])}/bin/python -m thespian.director shutdown";
+        ExecStop="${pkgs.python37.withPackages (pp: [ pp.thespian pp.setproctitle pp.requests ])}/bin/python -m thespian.director shutdown";
       };
     };
   };
