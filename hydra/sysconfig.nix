076ddffad2fffbf04d575ffa14ef715ac4527b8c
Kevin Quick <kquick@galois.com>
2020-02-15 09:47:13 -0800

    Place update date on HTML status summary in a div with gendate id.

--- hydra/sysconfig.nix
+++ hydra/sysconfig.nix
@@ -76,7 +76,7 @@ let
              cp ${briareus}/html/status_hdr.html $htmlout
              chmod +w $htmlout
              ${briareus}/bin/hh_status -f html -U ${builder_URL} ${name}.hhr >> $htmlout
-             echo "<br><hr><p><i>Updated: $(date)</i></p>" >>  $htmlout
+             echo "<div id=\"gendate\">Updated: $(date)</div>" >>  $htmlout
              echo '</body></html>' >> $htmlout
              set +x
       '';
