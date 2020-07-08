dbedee72bb45018287c24f1e0a7860642c429964
Kevin Quick <kquick@galois.com>
2020-07-07 23:27:36 -0700

    Update nix shell to include mypy and pytest coverage.

--- shell.nix
+++ shell.nix
@@ -7,12 +7,13 @@ let
 
   prolog = builtins.head (builtins.filter (d: d.pname == "swi-prolog") briareus.buildInputs);
 
+  testing = pkgs.python37.withPackages(pp: [pp.pytest pp.pytestcov pp.mypy]);
 in
 pkgs.lib.overrideDerivation briareus (drv: {
   src = ./.;
   shellHook = ''
     # Not entirely sure why swiProlog isn't added automatically.
-    export PATH=${prolog}/bin:${pkgs.python37Packages.pytest}/bin:$PATH
+    export PATH=${prolog}/bin:${testing}/bin:$PATH
     # Facilitate running pytest or local hh runs.
     export PYTHONPATH=$(pwd):$PYTHONPATH
   '';
