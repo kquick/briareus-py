7a8d3c1e336b407f3e822239738183949e3a6b17
Kevin Quick <kquick@galois.com>
2020-07-07 23:27:36 -0700

    Update default.nix and shell.nix for locally-generated swi-prolog with openssl 1.1.

--- default.nix
+++ default.nix
@@ -1,5 +1,22 @@
-{ stdenv, fetchFromGitHub, python3Packages, swiProlog, thespian, setproctitle, attrs, requests }:
+{ stdenv, fetchFromGitHub, python3Packages
+# , swiProlog
+, thespian, setproctitle, attrs, requests
+, openssl_1_1, pkgs
+}:
 
+let
+  # prolog = swiProlog.overrideAttrs (oldAttrs: {
+  #   openssl = openssl_1_1;
+  # });
+  # The above doesn't remove dependency on openssl_1_0_2, so use the following:
+
+  prolog = let l = <nixpkgs/pkgs/development/compilers/swi-prolog>;
+           in pkgs.callPackage l {
+             openssl = openssl_1_1;
+             Security = null;
+           };
+
+in
 python3Packages.buildPythonApplication rec {
   pname = "briareus";
   version = "0.1";
@@ -15,14 +32,16 @@ python3Packages.buildPythonApplication rec {
   #   sha256 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
   # };
 
-  propagatedBuildInputs =
-    let ppkgs = [
-          thespian
-          setproctitle
-          attrs
-          requests
-        ];
-    in [swiProlog] ++ ppkgs;
+  propagatedBuildInputs = [
+    thespian
+    setproctitle
+    attrs
+    requests
+  ];
+
+  buildInputs = [
+    prolog
+  ];
 
   checkInputs = propagatedBuildInputs;
   doCheck = false;
--- shell.nix
+++ shell.nix
@@ -4,12 +4,15 @@ let
   callPackage = pkgs.newScope (pkgs // pkgs.python37Packages);
 
   briareus = callPackage ./default.nix {};
+
+  prolog = builtins.head (builtins.filter (d: d.pname == "swi-prolog") briareus.buildInputs);
+
 in
 pkgs.lib.overrideDerivation briareus (drv: {
   src = ./.;
   shellHook = ''
     # Not entirely sure why swiProlog isn't added automatically.
-    export PATH=${pkgs.swiProlog}/bin:${pkgs.python37Packages.pytest}/bin:$PATH
+    export PATH=${prolog}/bin:${pkgs.python37Packages.pytest}/bin:$PATH
     # Facilitate running pytest or local hh runs.
     export PYTHONPATH=$(pwd):$PYTHONPATH
   '';
