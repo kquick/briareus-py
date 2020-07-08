2817765f26ca48dfba6e9b626a4b5c284ab4cada
Kevin Quick <kquick@galois.com>
2020-07-07 23:26:29 -0700

    Use attrs auto_attribs and standard type annotations in Briareus.Types.

--- Briareus/Types.py
+++ Briareus/Types.py
@@ -4,179 +4,185 @@
 # values defined here via the logic_result_expr translation below.
 
 import attr
+from typing import (Any, Dict, Hashable, List, Optional, Sequence, Union)
 
-def sorted_nub_list(l):
+def sorted_nub_list(l: Sequence[Hashable]) -> List[Hashable]:
     return sorted(list(set(l)))
 
-@attr.s(frozen=True)
+
+BldDescTy = Union["PR_Solo", "PR_Repogroup", "PR_Grouped",
+                  "BranchReq", "MainBranch", str]
+
+
+@attr.s(auto_attribs=True, frozen=True)
 class BldConfig(object):
-    projectname = attr.ib()
-    branchtype = attr.ib()  # default="regular"
-    branchname = attr.ib()
-    strategy   = attr.ib()  # default="standard"
-    description= attr.ib()  # PR_Solo, PR_Repogroup, PR_Grouped, BranchReq, MainBranch (or string placeholder)
-    blds       = attr.ib(factory=list, converter=sorted_nub_list)  # list of BldRepoRev
-    bldvars    = attr.ib(factory=list, converter=sorted_nub_list)
+    projectname: str
+    branchtype: str  # default="regular"
+    branchname: str
+    strategy: str   # default="standard"
+    description: BldDescTy
+    blds: List["BldRepoRev"] = attr.ib(factory=list, converter=sorted_nub_list)
+    bldvars: List[str] = attr.ib(factory=list, converter=sorted_nub_list)
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class BranchReq(object):
-    projectname = attr.ib() # string
-    branchname  = attr.ib() # string
+    projectname: str
+    branchname: str
 
-    def as_fact(self):
+    def as_fact(self) -> str:
         return 'branchreq("' + self.projectname + '", "' + self.branchname + '")'
 
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class MainBranch(object):
-    reponame = attr.ib() # string
-    branchname  = attr.ib() # string
+    reponame: str
+    branchname: str
 
-    def as_fact(self):
+    def as_fact(self) -> str:
         return 'main_branch("' + self.reponame + '", "' + self.branchname + '")'
 
 def pr_type(typespec, *args):
     return eval(typespec)(*args)
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PR_Solo(object):
-    projectname = attr.ib() # string
-    reponame    = attr.ib() # string
-    pullreq_id  = attr.ib() # string, never project_primary
+    projectname: str
+    reponame: str
+    pullreq_id: str # never project_primary
 
-    def as_fact(self):
+    def as_fact(self) -> str:
         return ('pr_type(pr_solo, "' + self.projectname +
                 '", "' + self.reponame +
                 '", "' + self.pullreq_id + '")')
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PR_Repogroup(object):
-    projectname = attr.ib() # string
-    pullreq_id  = attr.ib() # string, never project_primary
-    reponames   = attr.ib() # [string]
+    projectname: str
+    pullreq_id: str  # never project_primary
+    reponames: List[str]
 
-    def as_fact(self):
+    def as_fact(self) -> str:
         return ('pr_type(pr_repogroup, "' + self.projectname +
                 '", "' + self.pullreq_id +
                 '", ['
                 + ', '.join(['"%s"'%n for n in self.reponames]) + '])')
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PR_Grouped(object):
-    branchname = attr.ib() # string
+    branchname: str
 
-    def as_fact(self):
+    def as_fact(self) -> str:
         return 'pr_type(pr_grouped, "' + self.branchname + '")'
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class BldRepoRev(object):
-    reponame   = attr.ib()
-    repover    = attr.ib()
-    pullreq_id = attr.ib()  # project_primary or the PR ID
+    reponame: str
+    repover: str
+    pullreq_id: str  # project_primary or the PR ID
       # srcident identifies the source loc in the Logic rules and is
       # used for debugging the ruleset.  As a result, it is ignored
       # for comparisons since a specific repo revision build
       # instruction is effective in the builder regardless of which
       # logic statement generated it.
-    srcident   = attr.ib(default="unk", eq=False)
+    srcident: str = attr.ib(default="unk", eq=False)
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class BldVariable(object):
-    project = attr.ib()
-    varname = attr.ib()
-    varvalue = attr.ib()
+    project: str
+    varname: str
+    varvalue: str
 
 
 # ----------------------------------------------------------------------
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class BuildResult(object):
-    bldconfig = attr.ib()  # BldConfig
-    results = attr.ib()    # BuilderResult
+    bldconfig: BldConfig
+    results: "BuilderResult"
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class BuilderResult(object):
-    buildname   = attr.ib()  # string name of build on builder
-    nrtotal     = attr.ib()  # int
-    nrsucceeded = attr.ib()  # int
-    nrfailed    = attr.ib()  # int
-    nrscheduled = attr.ib()  # int
-    cfgerror    = attr.ib()  # bool
-    builder_url = attr.ib()  # string URL for this builder result
+    buildname:str  # string name of build on builder
+    nrtotal: int
+    nrsucceeded: int
+    nrfailed: int
+    nrscheduled: int
+    cfgerror: bool
+    builder_url: str
 
 
 # ----------------------------------------------------------------------
 # Reporting
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class ProjectSummary(object):
-    project_name = attr.ib()
-    bldcfg_count = attr.ib()  # int
-    subrepo_count = attr.ib()  # int
-    pullreq_count = attr.ib()  # int
+    project_name: str
+    bldcfg_count: int
+    subrepo_count: int
+    pullreq_count: int
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class StatusReport(object):
-    status    = attr.ib()  # string "succeeded", "fixed", "initial_success", "pending", "badconfig", or int count of failing build jobs.
-    project   = attr.ib()  # string name of project
-    strategy  = attr.ib()  # string: submodules, heads, standard
-    branchtype= attr.ib()  # string: regular, pullreq
-    branch    = attr.ib()  # string: branch name
-    buildname = attr.ib()  # string name of build on builder
-    bldvars   = attr.ib(converter=sorted)  # list of BldVariable
-    blddesc   = attr.ib(default="unk")  # same as BldConfig.description above
+    status: Union[str, int]  # string "succeeded", "fixed", "initial_success", "pending", "badconfig", or int count of failing build jobs.
+    project: str     # name of project
+    strategy: str    # submodules, heads, standard
+    branchtype: str  # regular, pullreq
+    branch: str      # branch name
+    buildname: str   # name of build on builder
+    bldvars: List[BldVariable] = attr.ib(converter=sorted)
+    blddesc: BldDescTy = attr.ib(default="unk")  # same as BldConfig.description above
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PendingStatus(object):
     """Just like StatusReport, but no 'status' field because there will
        (maybe) be a StatusReport from a previous build and this
        indicates there is a new build in progress.
     """
-    project   = attr.ib()  # string name of project
-    strategy  = attr.ib()  # string: submodules, heads, standard
-    branchtype= attr.ib()  # string: regular, pullreq
-    branch    = attr.ib()  # string: branch name
-    buildname = attr.ib()  # string name of build on builder
-    bldvars   = attr.ib(converter=sorted)  # list of BldVariable
-    blddesc   = attr.ib(default="unk")  # same as BldConfig.description above
+    project: str     # name of project
+    strategy: str    # submodules, heads, standard
+    branchtype: str  # regular, pullreq
+    branch: str      # branch name
+    buildname: str   # name of build on builder
+    bldvars: List[BldVariable] = attr.ib(converter=sorted)
+    blddesc: BldDescTy = attr.ib(default="unk")  # same as BldConfig.description above
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class NewPending(object):
-    bldcfg = attr.ib()  # BldConfig
+    bldcfg: BldConfig
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class VarFailure(BldVariable): pass
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PR_Status(object):
-    prtype = attr.ib() # PR_Solo, PR_Repogroup, or PR_Grouped
-    branch = attr.ib() # string
-    project = attr.ib() # string
-    strategy = attr.ib() # string: submodules, heads, standard
-    prcfg = attr.ib() # list of PRCfg or BranchCfg
-    prstatus_blds = attr.ib() # PR_Status_Bld objects
+    prtype: Union[PR_Solo, PR_Repogroup, PR_Grouped]
+    branch: str
+    project: str
+    strategy: str  # submodules, heads, standard
+    prcfg: List[Union["PRCfg", "BranchCfg"]]
+    prstatus_blds: "PR_Status_Blds"
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PR_Status_Blds(object):
-    passing = attr.ib() # list of passing BldNames
-    failing = attr.ib() # list of failing BldNames
-    pending = attr.ib() # list of pending BldNames (i.e. in-progress)
-    unstarted = attr.ib() # number of builds not yet started
+    passing: List[str]  # list of passing BldNames
+    failing: List[str]  # list of failing BldNames
+    pending: List[str]  # list of pending BldNames (i.e. in-progress)
+    unstarted: int      # number of builds not yet started
 
 fact_str = lambda s: '"' + s + '"'
 fact_list = lambda l: '[' + ', '.join([e.as_fact() if hasattr(e, 'as_fact') else fact_str(e)
                                        for e in l]) + ']'
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PRCfg(object):
-    reponame = attr.ib() # string
-    pr_id    = attr.ib() # string
-    branch   = attr.ib() # string
-    revision = attr.ib() # string head revision (e.g. SHA hash) or blank if not known
-    user     = attr.ib() # string
-    email    = attr.ib() # string
+    reponame: str
+    pr_id: str
+    branch: str
+    revision: str # head revision (e.g. SHA hash) or blank if not known
+    user: str
+    email: str
 
-    def as_fact(self):
+    def as_fact(self) -> str:
         return ''.join(['prcfg(',
                         ','.join([
                             fact_str(self.reponame),
@@ -188,40 +194,40 @@ class PRCfg(object):
                             ]),
                         ')'])
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class BranchCfg(object):
-    reponame = attr.ib() # string
-    branch   = attr.ib() # string
+    reponame: str
+    branch: str
 
-    def as_fact(self):
+    def as_fact(self) -> str:
         return ''.join(['branchcfg(',
                         fact_str(self.reponame), ',',
                         fact_str(self.branch),
                         ')'])
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PRData(object):
-    prtype = attr.ib() # PR_Solo, PR_Repogroup, or PR_Grouped
-    prcfg = attr.ib() # list of PRCfg or BranchCfg
+    prtype: Union[PR_Solo, PR_Repogroup, PR_Grouped]
+    prcfg: List[Union[PRCfg, BranchCfg]]
 
-    def as_fact(self):
+    def as_fact(self) -> str:
         return ''.join(['prdata(',
                         self._as_fact_fields(),
                         ')'])
 
-    def _as_fact_fields(self):
+    def _as_fact_fields(self) -> str:
         return ', '.join([self.prtype.as_fact(),
                           fact_list(self.prcfg),
         ])
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class BldSet(object):
-    brtype = attr.ib() # "pullreq" or "regular"
-    strategy = attr.ib() # "submodules", "heads" or "regular"
-    goods = attr.ib(converter=sorted) # list of BuildNames
-    fails = attr.ib(converter=sorted) # list of BuildNames
+    brtype: str    # "pullreq" or "regular"
+    strategy: str  # "submodules", "heads" or "regular"
+    goods: List[str] = attr.ib(converter=sorted) # list of BuildNames
+    fails: List[str] = attr.ib(converter=sorted) # list of BuildNames
 
-    def as_fact(self):
+    def as_fact(self) -> str:
         return ('bldset(' +
                 ', '.join([self.brtype,
                            self.strategy,
@@ -230,14 +236,14 @@ class BldSet(object):
                 ]) +
                 ')')
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PRFailedSubBlds(PRData):
-    pr_subs = attr.ib()    # BldSet for pullreq submodules
-    pr_heads = attr.ib()   # BldSet for pullreq heads
-    main_heads = attr.ib() # BldSet for main branch heads
-    main_subs = attr.ib()  # BldSet for main branch submodules
+    pr_subs: BldSet    # BldSet for pullreq submodules
+    pr_heads: BldSet   # BldSet for pullreq heads
+    main_heads: BldSet # BldSet for main branch heads
+    main_subs: BldSet  # BldSet for main branch submodules
 
-    def as_fact(self):
+    def as_fact(self) -> str:
         return ('prfailedblds(' +
                 ', '.join([super()._as_fact_fields(),
                            self.pr_subs.as_fact(),
@@ -247,12 +253,12 @@ class PRFailedSubBlds(PRData):
                 ]) +
                 ')')
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PRFailedStdBlds(PRData):
-    pr_blds = attr.ib()    # list of BldSet for pullreq standard
-    main_blds = attr.ib() # list of BldSet for main branch standard
+    pr_blds: BldSet    # BldSet for pullreq standard
+    main_blds: BldSet  # BldSet for main branch standard
 
-    def as_fact(self):
+    def as_fact(self) -> str:
         return ('prfailedblds(' +
                 ', '.join([super()._as_fact_fields(),
                            self.pr_blds.as_fact(),
@@ -260,7 +266,7 @@ class PRFailedStdBlds(PRData):
                 ]) +
                 ')')
 
-def pr_failedblds(*args):
+def pr_failedblds(*args) -> Union[PRFailedStdBlds, PRFailedSubBlds]:
     if len(args) == 4:
         return PRFailedStdBlds(*args)
     elif len(args) == 6:
@@ -268,46 +274,46 @@ def pr_failedblds(*args):
     else:
         raise IndexError('pr_faildblds with %d args not implemented' % len(args))
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class CompletelyFailing(object):
-    project = attr.ib() # string name of project
+    project: str # name of project
 
 # ----------------------------------------------------------------------
 # Analysis
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class SepHandledVar(object):
-    project = attr.ib() # string: project repo
-    var_name = attr.ib() # string: variable name
-    var_value = attr.ib() # string: variable value
+    project: str   # project repo
+    var_name: str  # variable name
+    var_value: str # variable value
 
 # ----------------------------------------------------------------------
 # Actions
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class Notify(object):
-    what = attr.ib() # string: type of notification
-    subject = attr.ib() # string: project
-    params = attr.ib() # parameters associated with item; specific to 'what'
+    what: str         # type of notification
+    subject: str      # project
+    params: List[Any] # parameters associated with item; specific to 'what'
 
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class SendEmail(object):
-    recipients = attr.ib(converter=sorted_nub_list) # list of email addresses
-    notification = attr.ib() # Notify object
-    sent_to = attr.ib(converter=sorted_nub_list) # list of addresses message has been sent to already
+    recipients: List[str] = attr.ib(converter=sorted_nub_list) # list of email addresses
+    notification: Notify
+    sent_to: List[str] = attr.ib(converter=sorted_nub_list) # list of addresses message has been sent to already
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PostChatMessage(object):
-    channels = attr.ib() # list of channels
-    what = attr.ib() # Notification type
-    posted = attr.ib() # list of channels message has been posted to already
+    channels: List[str]  # list of channels
+    what: Notify         # Notification type
+    posted: List[str]    # list of channels message has been posted to already
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class SetForgeStatus(object):
-    targetrepos = attr.ib(converter=sorted_nub_list) # list of repo names
-    notification = attr.ib() # Notify object
-    updated = attr.ib(converter=sorted_nub_list) # list of repo names posting has been made to
+    targetrepos: List[str] = attr.ib(converter=sorted_nub_list) # list of repo names
+    notification: Notify
+    updated: List[str] = attr.ib(converter=sorted_nub_list) # list of repo names posting has been made to
 
 # ----------------------------------------------------------------------
 # The Prolog output is currently interpreted via an "eval({output})"
@@ -377,20 +383,20 @@ logic_result_expr = {
 # ----------------------------------------------------------------------
 # Information accumulated and managed internally to Briareus
 
-@attr.s
+@attr.s(auto_attribs=True)
 class RunContext(object):
     "Stores the result of one or more generated build configurations"
 
-    actor_system = attr.ib()
-    result_sets = attr.ib(factory=list) # list of ResultSet
-    report = attr.ib(factory=list)  # list of report-generated items
+    actor_system: Any
+    result_sets: List["ResultSet"] = attr.ib(factory=list)
+    report: List[Any] = attr.ib(factory=list)  # list of report-generated items
 
-    def add_results(self, builder, inp_desc, repo_info, build_cfgs):
+    def add_results(self, builder, inp_desc, repo_info, build_cfgs) -> None:
         self.result_sets.append(ResultSet(builder, inp_desc, repo_info, build_cfgs))
 
 
 
-@attr.s
+@attr.s(auto_attribs=True)
 class ResultSet(object):
     """This object accumulates the various project-specific information
     obtained during the Briareus processing.
@@ -401,23 +407,23 @@ class ResultSet(object):
     # builders should be the same, and the first generated should be
     # threaded through, or the builder map should be passed to AnaRep
     # for consuting project-specific builders for results.
-    builder = attr.ib(default=None)
+    builder: Optional[Any] = attr.ib(default=None)  # Briareus.BuilderBase.Builder
 
     # inp_desc is an array of Briareus.Input.Description.InputDesc
     # object.  These are kept separate so that the set of input logic
     # facts are consistent to each inp_desc.
-    inp_desc = attr.ib(default=None)
+    inp_desc: Optional[List[Any]] = attr.ib(default=None)  # Briareus.Input.Description.InputDesc
 
     # repo_info is a dictionary gathered from the remote repositories.
     # Assume that the dictionaries can simply be combined and that any
     # duplicates are identical.  This might not be true if the same
     # name were chosen for different repositories, but we currently
     # deem that a bad input configuration.
-    repo_info = attr.ib(default=None)
+    repo_info: Dict[int, int] = attr.ib(default=None)
 
     # build_cfgs is the internal build configs,
     # Briareus.BCGen.Generator.GeneratedConfigs
-    build_cfgs = attr.ib(default=None)
+    build_cfgs: Optional[Any] = attr.ib(default=None) # Briareus.BCGen.Generator.GeneratedConfigs
 
     # build_results are the array of [BuildResult]
-    build_results = attr.ib(factory=list)
+    build_results: List[BuildResult] = attr.ib(factory=list)
