712a1e2ffce1776343906bbbd05daf2fcd3bb5be
Kevin Quick <kquick@galois.com>
2020-07-07 23:22:41 -0700

    Add type annotations to VCS InternalMessages.

--- Briareus/VCS/InternalMessages.py
+++ Briareus/VCS/InternalMessages.py
@@ -3,8 +3,9 @@
 import attr
 import json
 from Briareus.Input.Description import *
+from typing import (Dict, List, Optional, Tuple, Union)
 
-def toJSON(obj):
+def toJSON(obj) -> str:
     class objToJSON(json.JSONEncoder):
         def default(self, obj):
             if obj.__class__.__name__ in [ 'dict', 'list', 'int', 'float',
@@ -19,7 +20,7 @@ def toJSON(obj):
             return objdict
     return json.dumps(obj, cls=objToJSON)
 
-def fromJSON(jstr):
+def fromJSON(jstr: str):
     def objFromJSON(objdict):
         if '__type__' in objdict:
             objtype = objdict['__type__']
@@ -31,148 +32,151 @@ def fromJSON(jstr):
     return json.loads(jstr, object_hook=objFromJSON)
 
 
-@attr.s
+@attr.s(auto_attribs=True)
 class GatherInfo(object):             #            --> GatheredInfo
-    repolist = attr.ib(factory=list)    # array of RepoDesc
-    repolocs = attr.ib(factory=list)    # array of RepoLoc
-    branchlist = attr.ib(factory=list)  # array of BranchDesc
-@attr.s
+    repolist: List[RepoDesc] = attr.ib(factory=list)
+    repolocs: List[RepoLoc]  = attr.ib(factory=list)
+    branchlist: List[BranchDesc] = attr.ib(factory=list)
+@attr.s(auto_attribs=True)
 class GatheredInfo(object):           # GatherInfo -->
-    info = attr.ib(factory=dict)
-    error = attr.ib(default=None)
+    info: Dict[int, int] = attr.ib(factory=dict)
+    error: Optional[str] = attr.ib(default=None)
 
 
-@attr.s
+@attr.s(auto_attribs=True)
 class ReadFileFromVCS(object):        # --> FileReadData
-    repourl  = attr.ib()
-    repolocs = attr.ib()              # array of RepoLoc
-    file_path = attr.ib()
-    branch = attr.ib(factory=str)     # defaults to "master"
-@attr.s
+    repourl: str
+    repolocs: List[RepoLoc]
+    file_path: str
+    branch: str = attr.ib(factory=str)     # defaults to "master"
+@attr.s(auto_attribs=True)
 class FileReadData(object):           # ReadFileFromVCS -->
-    req = attr.ib()                     # The ReadFileFromVCS that originated this response
-    error_code = attr.ib(default=None)  # integer, 0/None on success
-    file_data = attr.ib(factory=str)    # string, empty if error_code
+    req: ReadFileFromVCS                # The ReadFileFromVCS that originated this response
+    error_code: Optional[int] = attr.ib(default=None)  # integer, 0/None on success
+    file_data: str = attr.ib(factory=str)    # empty string if error_code
 
 
-@attr.s
+@attr.s(auto_attribs=True)
 class Repo__ReqMsg(object):
-    reponame = attr.ib()
-@attr.s
+    reponame: str
+@attr.s(auto_attribs=True)
 class Repo__RspMsg(object):
-    reponame = attr.ib()
+    reponame: str
 
 
-@attr.s
+@attr.s(auto_attribs=True)
 class Repo_AltLoc_ReqMsg(object):
     """Sends a Repo__ReqMsg but the target should handle it at an
        alternate repository that is associated with the main reponame
        but is not the main repo (e.g. a pullreq source repo).
     """
-    api_repo_loc = attr.ib()   # API URL specification for alt repo
-    altloc_reqmsg = attr.ib()  # Repo__ReqMsg to send to the alt repo loc
+    api_repo_loc: str   # API URL specification for alt repo
+    altloc_reqmsg: Repo__ReqMsg  # Repo__ReqMsg to send to the alt repo loc
 
 
-@attr.s
+@attr.s(auto_attribs=True)
 class DeclareRepo(Repo__ReqMsg):        #             --> RepoDeclared
-    repo_url = attr.ib()
-    repolocs = attr.ib(factory=list)    # array of RepoLoc
+    repo_url: str
+    repolocs: List[RepoLoc] = attr.ib(factory=list)
 @attr.s
 class RepoDeclared(Repo__RspMsg):       # DeclareRepo -->
     pass
 
 
-@attr.s
+@attr.s(auto_attribs=True)
 class InvalidRepo(Repo__RspMsg):        # {ANY} -->
-    repo_type = attr.ib()
-    repo_remote = attr.ib()
-    repo_api_url = attr.ib()
-    errorstr = attr.ib()
+    repo_name: str
+    repo_type: str
+    repo_api_url: str
+    errorstr: str
 
 
-class GetPullReqs(Repo__ReqMsg): pass   #             --> PullReqInfo
-@attr.s
-class PullReqsData(Repo__RspMsg):       # GetPullReqs -->
-    pullreqs = attr.ib(factory=list)    # list of PullReqInfo
-
-@attr.s
-class PullReqInfo(object):
-    pullreq_number = attr.ib()
-    pullreq_status = attr.ib() # PullReqStatus__Base derivation
-    pullreq_title  = attr.ib()
-    pullreq_srcurl = attr.ib()
-    pullreq_branch = attr.ib()
-    pullreq_ref    = attr.ib()
-    pullreq_user   = attr.ib() # string name of user on forge
-    pullreq_email  = attr.ib() # string user email (may be blank if not public)
-    pullreq_mergeref = attr.ib(default=None) # if available
-
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PullReqStatus__Base(object):
-    def as_fact(self):
+    def as_fact(self) -> str:
         return self.__class__.__name__.lower()
 class PRSts_Active(PullReqStatus__Base): pass
 class PRSts_Closed(PullReqStatus__Base): pass
 class PRSts_Merged(PullReqStatus__Base): pass
 
-@attr.s
+@attr.s(auto_attribs=True)
+class PullReqInfo(object):
+    pullreq_number: str
+    pullreq_status: PullReqStatus__Base # derivation
+    pullreq_title: str
+    pullreq_srcurl: str
+    pullreq_branch: str
+    pullreq_ref: str
+    pullreq_user: str   # name of user on forge
+    pullreq_email: str  # user email (may be blank if not public)
+    pullreq_mergeref: Optional[str] = attr.ib(default=None) # if available
+
+class GetPullReqs(Repo__ReqMsg): pass   #             --> PullReqInfo
+@attr.s(auto_attribs=True)
+class PullReqsData(Repo__RspMsg):       # GetPullReqs -->
+    pullreqs: List[PullReqInfo] = attr.ib(factory=list)
+
+
+@attr.s(auto_attribs=True)
 class HasBranch(Repo__ReqMsg):          #           --> BranchPresent
-    branch_name = attr.ib()
-@attr.s
+    branch_name: str
+@attr.s(auto_attribs=True)
 class BranchPresent(Repo__RspMsg):      # HasBranch -->
-    branch_name = attr.ib()
-    branch_present = attr.ib(default=False)  # either False or the current branch head ref (SHA)
-    known_branches = attr.ib(factory=list)   # array of tuples: (name,head_ref)
+    branch_name: str
+    branch_present: Optional[Union[bool,str]] = attr.ib(default=False)  # either False or the current branch head ref (SHA)
+    known_branches: List[Tuple[str,str]] = attr.ib(factory=list)  # list of tuples: (name,head_ref)
 
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class BranchRef(object):
-    reponame = attr.ib() # string
-    branchname = attr.ib() # string
-    branchref = attr.ib() # string reference (e.g. SHA)
+    reponame: str
+    branchname: str
+    branchref: str   # reference (e.g. SHA)
 
 
-@attr.s
-class GitmodulesData(Repo__ReqMsg):     #                --> GitmodulesRepoVers
-    branch_name = attr.ib()
-    pullreq_id = attr.ib()  # may be none, but distinguishes between
-                            # PR's from master branches.  The ID was
-                            # chosen to be unique relative to the
-                            # source repo; another unique field
-                            # (e.g. pullreq_srcurl) could
-                            # alternatively be used.
-    source_ref = attr.ib()  # explicit ref for src (e.g. for Gitlab)
-@attr.s
-class GitmodulesRepoVers(Repo__RspMsg): # GitmodulesData -->
-    branch_name = attr.ib()
-    pullreq_id = attr.ib()  # may be none, but distinguishes between PR's from master branches
-    gitmodules_repovers = attr.ib(factory=list)  # array of SubRepoVers
-
-@attr.s
+@attr.s(auto_attribs=True)
 class SubRepoVers(object):
-    subrepo_name = attr.ib()
-    subrepo_url  = attr.ib()
-    subrepo_vers = attr.ib()
+    subrepo_name: str
+    subrepo_url: str
+    subrepo_vers: str
 
-@attr.s
+@attr.s(auto_attribs=True)
+class GitmodulesData(Repo__ReqMsg):     #                --> GitmodulesRepoVers
+    branch_name: str
+    pullreq_id: Optional[str]  # may be none, but distinguishes between
+                               # PR's from master branches.  The ID was
+                               # chosen to be unique relative to the
+                               # source repo; another unique field
+                               # (e.g. pullreq_srcurl) could
+                               # alternatively be used.
+    source_ref: str  # explicit ref for src (e.g. for Gitlab)
+@attr.s(auto_attribs=True)
+class GitmodulesRepoVers(Repo__RspMsg): # GitmodulesData -->
+    branch_name: str
+    pullreq_id: Optional[str]  # may be none, but distinguishes
+                               # between PR's from master branches
+    gitmodules_repovers: List[SubRepoVers] = attr.ib(factory=list)
+
+
+@attr.s(auto_attribs=True)
 class RepoRemoteSpec(object):
-    repo_api_loc = attr.ib()               # RepoAPI_Location for forge API
+    repo_api_loc: str               # RepoAPI_Location for forge API
 
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class PRInfo(object):
-    pr_target_repo = attr.ib()
-    pr_srcrepo_url = attr.ib()
-    pr_branch      = attr.ib()
-    pr_revision    = attr.ib()
-    pr_ident       = attr.ib()  # unique identifier string, required
-    pr_status      = attr.ib()  # PullReqStatus__Base derivation
-    pr_title       = attr.ib()  # user-assistance, optional
-    pr_user        = attr.ib()  # name of user creating pull request
-    pr_email       = attr.ib()  # email of user (if known, else blank)
+    pr_target_repo: str
+    pr_srcrepo_url: str
+    pr_branch: str
+    pr_revision: str
+    pr_ident: str  # unique identifier string, required
+    pr_status: PullReqStatus__Base  # derivation
+    pr_title: str  # user-assistance, optional
+    pr_user: str   # name of user creating pull request
+    pr_email: str  # email of user (if known, else blank)
 
 
-@attr.s(frozen=True)
+@attr.s(auto_attribs=True, frozen=True)
 class SubModuleInfo(object):
     """Describes a known submodule for a repository.  Only a project
        repository is checked for submodules.
@@ -193,8 +197,8 @@ class SubModuleInfo(object):
        is used to uniquely differentiante between these).
 
     """
-    sm_repo_name = attr.ib()  # Primary repo name (the one that contains the submodule)
-    sm_branch    = attr.ib()  # Submodule branch name (in source repo)
-    sm_pullreq_id= attr.ib()  # If submodule for a PR to the project repo, this identifies the PR
-    sm_sub_name  = attr.ib()  # Submodule repo name
-    sm_sub_vers  = attr.ib()  # Submodule repo specified version
+    sm_repo_name: str  # Primary repo name (the one that contains the submodule)
+    sm_branch: str     # Submodule branch name (in source repo)
+    sm_pullreq_id: str # If submodule for a PR to the project repo, this identifies the PR
+    sm_sub_name: str   # Submodule repo name
+    sm_sub_vers: str   # Submodule repo specified version
